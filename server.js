3609c44766ea267983caa2d4485062ed151176ca
Christina Labay <Christina.m.labay@gmail.com>
2019-04-23 05:40:04 -0500

    ready for production

--- server.js
+++ server.js
@@ -7,7 +7,8 @@ const cors = require('cors')
 
 const app = express()
 
-app.use(morgan('dev'))
+const mogranSetting = process.send.NODE_ENV === 'production' ? 'tiny' : 'common'
+app.use(morgan(morganSetting))
 app.use(cors())
 app.use(helmet())
 
@@ -23,13 +24,6 @@ app.use((function validateBearerToken(req, res, next){
     //move to the next
     next()
 }));
-
-//1. The endpoint is GET /movie
-//2. The search options for either name or type are provided in query string parameters.
-//3. When searching by genre, users are searching for whether the Movie's genre includes a specified string. The search should be case insensitive.
-//4. When searching by country, users are searching for whether the Movie's country includes a specified string. The search should be case insensitive.
-//5. When searching by average vote, users are searching for Movies with an avg_vote that is greater than or equal to the supplied number.
-//6. The API responds with an array of full movie entries for the search results
 
 function handleGetMovies(req, res){
     const { genre, country, avg_vote} = req.query;
@@ -55,7 +49,17 @@ function handleGetMovies(req, res){
 
 app.get('/movie', handleGetMovies)
 
-const PORT = 8000
+app.use((error, req, res, next) => {
+    let response
+    if(process.env.NODE_ENV === 'production'){
+        response = { error: { message: 'server error' }}
+    } else {
+        response = { error }
+    }
+    res.status(500).json(response)
+})
+
+const PORT = process.send.PORT || 8000
 
 app.listen(PORT, () => {
     console.log(`Server listening at http://localhost:${PORT}`)
